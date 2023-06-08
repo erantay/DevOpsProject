1d2c64f7c48146d027e26b235a4750abfb2be5ab
Eran Tayter <tlvtoto@gmail.com>
2023-06-08 18:47:31 +0300

    testing

--- tests/e2e.py
+++ tests/e2e.py
@@ -1,7 +1,25 @@
+from selenium import webdriver
+
+
 def test_scores_service(application_url):
-    return 1
+    try:
+        driver = webdriver.Safari()
+        driver.get(application_url)
+        score = driver.find_element(by="id", value="score").text
+        if 1 <= int(score) <= 1000:
+            return 1
+        else:
+            return 0
+    except BaseException as e:
+        # print(e)
+        driver.close()
+        return 0
+    finally:
+        driver.close()
 
 
 def main_function():
-    return 1
+    return test_scores_service('http://localhost:8080/score')
 
+
+print(main_function())
