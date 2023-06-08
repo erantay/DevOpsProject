86b77441e95e8f020a00e239d783747655717c95
Eran Tayter <tlvtoto@gmail.com>
2023-06-08 18:59:15 +0300

    testing fix

--- tests/e2e.py
+++ tests/e2e.py
@@ -1,25 +1,25 @@
-from selenium import webdriver
+import sys
 
+from selenium import webdriver
 
 def test_scores_service(application_url):
     try:
         driver = webdriver.Safari()
         driver.get(application_url)
         score = driver.find_element(by="id", value="score").text
-        if 1 <= int(score) <= 1000:
-            return 1
-        else:
-            return 0
+        return 1 <= int(score) <= 1000
     except BaseException as e:
         # print(e)
-        driver.close()
-        return 0
+        return False
     finally:
         driver.close()
 
 
 def main_function():
-    return test_scores_service('http://localhost:8080/score')
+    test = test_scores_service('http://localhost:8080/score')
+    result = -1 if test else 0
+
+    sys.exit(result)
 
 
-main_function()
+exit(main_function())
